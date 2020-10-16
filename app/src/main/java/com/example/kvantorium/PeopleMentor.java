a2484e0cf7951683bf98619eecc8c39281ee67e5
PritOriginal <stepan01062004@mail.ru>
2020-10-16 17:45:06 +0300

    Добавлена сортировка по группам

--- app/src/main/java/com/example/kvantorium/PeopleMentor.java
+++ app/src/main/java/com/example/kvantorium/PeopleMentor.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 import com.example.kvantorium.server.GetStudents;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -39,7 +41,7 @@ public class PeopleMentor extends Fragment implements OnUsersListener {
     }
 
     public void sorting() {
-        ArrayList<Integer> group = new ArrayList<Integer>();
+        final ArrayList<Integer> group = new ArrayList<Integer>();
         ArrayList<ArrayList<User>> users_group = new ArrayList<ArrayList<User>>();
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getGroup() != 0) {
@@ -54,6 +56,7 @@ public class PeopleMentor extends Fragment implements OnUsersListener {
                         ArrayList<User> users_ = new ArrayList<User>();
                         users_.add(users.get(i));
                         users_group.add(users_);
+                        break;
                     }
                     j++;
                 }
@@ -66,12 +69,46 @@ public class PeopleMentor extends Fragment implements OnUsersListener {
                //group.add(users.get(i).getGroup());
             }
         }
+/*
+        Collections.sort(users_group, new Comparator<ArrayList<ArrayList<User>>>() {
+
+            @Override
+            public int compare(ArrayList<ArrayList<User>> o1, ArrayList<ArrayList<User>> o2) {
+                return o1.get(i).compareTo(o2.indexOf(group));
+            }
+        });
+ */
+        ArrayList<ArrayList<User>> users_group_ = new ArrayList<ArrayList<User>>();
+        int g = Collections.min(group);
+        int h = 0;
+        for (int i = 0; i < users_group.size(); i++) {
+            /*
+            for (int j = 0; j < group.size(); j++) {
+                if (group.get(j) < g) {
+                    g = group.get(j);
+                    h = j;
+                }
+                if (j == users_group.size() - 1) {
+                    users_group_.add(users_group.get(group.indexOf(j)));
+                }
+            }
+             */
+            users_group_.add(users_group.get(group.indexOf(i+1)));
+            Collections.sort(users_group_.get(i), new Comparator<User>() {
+                @Override
+                public int compare(User o1, User o2) {
+                    return o1.getSecondName().compareTo(o2.getSecondName());
+                }
+            });
+        }
+        Collections.sort(group);
      //   System.out.println(users_group);
-        update(group, users_group);
+        update(group, users_group_);
     }
 
-    public void update(ArrayList<Integer> group, ArrayList<ArrayList<User>> users_group) {
+    public void update(final ArrayList<Integer> group, ArrayList<ArrayList<User>> users_group) {
         System.out.println(group.size() + "  и  " + users_group.size());
+
         for (int i = 0; i < users_group.size(); i++) {
             final View v1 = getLayoutInflater().inflate(R.layout.group_list, null);
             TextView t1 = (TextView) v1.findViewById(R.id.name_group);
@@ -79,6 +116,7 @@ public class PeopleMentor extends Fragment implements OnUsersListener {
             RecyclerView groupList = (RecyclerView) v1.findViewById(R.id.group_list);
             LinearLayoutManager llm = new LinearLayoutManager(main.getApplicationContext());
             groupList.setLayoutManager(llm);
+
             adapter = new RVAdapterUsers(main, users_group.get(i));
             groupList.setAdapter(adapter);
             linearLayout.addView(v1);
